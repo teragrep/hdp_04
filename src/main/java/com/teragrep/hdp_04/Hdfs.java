7e7416676501c3f14a96934e02ed480e270ece10
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-04-03 12:28:09 +0300

    Initial release

---
+++ src/main/java/com/teragrep/hdp_04/Hdfs.java
@@ -0,0 +1,75 @@
+/*
+ * Execution Requisites HDP-04
+ * Copyright (C) 2025 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://github.com/teragrep/teragrep/blob/main/LICENSE>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+package com.teragrep.hdp_04;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.security.UserGroupInformation;
+
+import java.io.IOException;
+
+public class Hdfs {
+
+    private final String configPath;
+    private final String principal;
+    private final String keytab;
+
+    public Hdfs(final String configPath, final String principal, final String keytab) {
+        this.configPath = configPath;
+        this.principal = principal;
+        this.keytab = keytab;
+    }
+
+    public FileSystem get() throws IOException {
+        final Configuration configuration = new Configuration();
+        configuration.addResource(new Path("file://" + configPath + "/core-site.xml"));
+        configuration.addResource(new Path("file://" + configPath + "/hdfs-site.xml"));
+        UserGroupInformation.setConfiguration(configuration);
+        UserGroupInformation.loginUserFromKeytab(principal, keytab);
+        return FileSystem.get(configuration);
+    }
+}
